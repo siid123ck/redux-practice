fbd56917d0a1cb0d3cdfd379b07a220418ec0940
siid sharma <siid123ck@gmail.com>
2021-01-05 17:14:37 +1100

    added handlers to store and delete result

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -4,6 +4,7 @@ import {connect} from "react-redux";
   class Counter extends React.Component {
    
     render(){
+        console.log(this.props.results)
         return (
             <div>
                 <h1>Counter value: {this.props.value} </h1>
@@ -11,19 +12,28 @@ import {connect} from "react-redux";
                 <button onClick={this.props.decreamentHandler}>Decrease by one</button>
                 <button onClick={this.props.addHandler}>Add random value</button>
                 <button onClick={this.props.subHandler}>Subtract random value</button>
+                <button onClick={this.props.storeHandler}>store result</button>
+                <ul>
+                    {this.props.results.map((result)=>(
+                        <li onClick={()=>this.props.deleteHandler(result.id)} key={result.id}> {result.val}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
 }
 
 const mapStateToProps = state =>({
-    value:state.count
+    value:state.count, 
+    results:state.results
 })
 const mapDispatchToProps = dispatch=>({
     increamentHandler:()=>dispatch({type:"INCREAMENT"}),
     decreamentHandler:()=>dispatch({type:"DECREAMENT"}),
     addHandler:()=>dispatch({type:"ADD", value:3}),
-    subHandler:()=>dispatch({type:"SUB", value:3})
+    subHandler:()=>dispatch({type:"SUB", value:3}),
+    storeHandler:()=>dispatch({type:"STORE_RESULT"}),
+    deleteHandler:(id)=>dispatch({type:"DELETE_RESULT", resultID:id} ),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    count:11
+    count:11, 
+    results:[]
 }
 
 const rootReducer = (state=initialState, action)=>{
@@ -14,6 +15,16 @@ const rootReducer = (state=initialState, action)=>{
     
         case "SUB":
             return {...state, count:state.count - action.value}
+    
+        case "STORE_RESULT":
+            return {
+                ...state, results:state.results.concat({val:state.count, id: new Date()})
+            }
+        case "DELETE_RESULT":
+            const updateResult = state.results.filter(result=>result.id !== action.resultID)
+            return {
+                ...state, results:updateResult
+            }
     
         default:
             return state;
